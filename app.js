4355785650427a7536c904dd89367fbb1d7c8e43
YuukiHoriuchi <y.horiuchi@backtech.co.jp>
2020-09-21 22:03:26 +0900

    クエリパラメーターのテスト

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const http = require('http');
 const fs = require('fs');
 const ejs = require('ejs');
- 
+const url = require('url');
+
 const index_page = fs.readFileSync('./index.ejs', 'utf8');
+const other_page = fs.readFileSync('./other.ejs', 'utf8');
+const style_css = fs.readFileSync('./style.css', 'utf8');
 
 var server = http.createServer(getFromClient);
 
@@ -12,12 +15,29 @@ console.log('Server start!');
 // ここまでメインプログラム==========
 
 // createServerの処理
-function getFromClient(request, response){
-  var content = ejs.render(index_page,{
-      title: "indexページ",
-      content: "テンプレートを使用しています。",
-  });
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  response.write(content);
-  response.end();
+function getFromClient(request, response) {
+
+  var url_parts = url.parse(request.url, true); //☆trueにする!
+  switch (url_parts.pathname) {
+
+    case '/':
+      var content = "これはIndexページです。"
+      var query = url_parts.query;
+      if (query.msg != undefined) {
+        content += 'あなたは、「' + query.msg + '」と送りました。';
+      }
+      var content = ejs.render(index_page, {
+        title: "Index",
+        content: content,
+      });
+      response.writeHead(200, { 'Content-Type': 'text/html' });
+      response.write(content);
+      response.end();
+      break;
+
+    default:
+      response.writeHead(200, { 'Content-Type': 'text/plain' });
+      response.end('no page...');
+      break;
+  }
 }
