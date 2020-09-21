734b0915890a25d09465d1df1e1db3d625c9949c
YuukiHoriuchi <y.horiuchi@backtech.co.jp>
2020-09-22 08:06:27 +0900

    メッセージボードの作成

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const http = require('http');
 const fs = require('fs');
 const ejs = require('ejs');
 const url = require('url');
-const qs = require('querystring'); //★追加
+const qs = require('querystring');
 
 const index_page = fs.readFileSync('./index.ejs', 'utf8');
-const other_page = fs.readFileSync('./other.ejs', 'utf8');
-const style_css = fs.readFileSync('./style.css', 'utf8');
+const login_page = fs.readFileSync('./login.ejs', 'utf8');
+
+const max_num = 10; // 最大保管数
+const filename = 'mydata.txt'; // データファイル名
+var message_data; // データ
+readFromFile(filename);
 
 var server = http.createServer(getFromClient);
 
@@ -17,22 +21,16 @@ console.log('Server start!');
 
 // createServerの処理
 function getFromClient(request, response) {
-  var url_parts = url.parse(request.url, true); //★trueに
 
+  var url_parts = url.parse(request.url, true);
   switch (url_parts.pathname) {
 
-    case '/':
-      response_index(request, response); //★修正
+    case '/': // トップページ（メッセージボード）
+      response_index(request, response);
       break;
 
-    case '/other':
-      response_other(request, response); //★修正
-      break;
-
-    case '/style.css':
-      response.writeHead(200, { 'Content-Type': 'text/css' });
-      response.write(style_css);
-      response.end();
+    case '/login': // ログインページ
+      response_login(request, response);
       break;
 
     default:
@@ -42,54 +40,73 @@ function getFromClient(request, response) {
   }
 }
 
-// ★indexのアクセス処理
+// loginのアクセス処理
+function response_login(request, response) {
+  var content = ejs.render(login_page, {});
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(content);
+  response.end();
+}
+
+// indexのアクセス処理
 function response_index(request, response) {
-  var msg = "これはIndexページです。"
+  // POSTアクセス時の処理
+  if (request.method == 'POST') {
+    var body = '';
+
+    // データ受信のイベント処理
+    request.on('data', function (data) {
+      body += data;
+    });
+
+    // データ受信終了のイベント処理
+    request.on('end', function () {
+      data = qs.parse(body);
+      addToData(data.id, data.msg, filename, request);
+      write_index(request, response);
+    });
+  } else {
+    write_index(request, response);
+  }
+}
+
+// indexのページ作成
+function write_index(request, response) {
+  var msg = "※何かメッセージを書いて下さい。";
   var content = ejs.render(index_page, {
-    title: "Index",
+    title: 'Index',
     content: msg,
+    data: message_data,
+    filename: 'data_item',
   });
   response.writeHead(200, { 'Content-Type': 'text/html' });
   response.write(content);
   response.end();
 }
 
-// ★otherのアクセス処理
-function response_other(request, response) {
-  var msg = "これはOtherページです。"
-
-  // POSTアクセス時の処理
-  if (request.method == 'POST') {
-    var body = '';
-
-    // データ受信のイベント処理
-    request.on('data', (data) => {
-      body += data;
-    });
-
-    // データ受信終了のイベント処理
-    request.on('end', () => {
-      var post_data = qs.parse(body); // ★データのパース
-      msg += 'あなたは、「' + post_data.msg + '」と書きました。';
-      var content = ejs.render(other_page, {
-        title: "Other",
-        content: msg,
-      });
-      response.writeHead(200, { 'Content-Type': 'text/html' });
-      response.write(content);
-      response.end();
-    });
-
-    // GETアクセス時の処理
-  } else {
-    var msg = "ページがありません。"
-    var content = ejs.render(other_page, {
-      title: "Other",
-      content: msg,
-    });
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.write(content);
-    response.end();
-  }
+// テキストファイルをロード
+function readFromFile(fname) {
+  fs.readFile(fname, 'utf8', (err, data) => {
+    message_data = data.split('\n');
+  })
 }
 
+// データを更新
+function addToData(id, msg, fname, request) {
+  var obj = { 'id': id, 'msg': msg };
+  var obj_str = JSON.stringify(obj);
+  console.log('add data: ' + obj_str);
+  message_data.unshift(obj_str);
+  if (message_data.length > max_num) {
+    message_data.pop();
+  }
+  saveToFile(fname);
+}
+
+// データを保存
+function saveToFile(fname) {
+  var data_str = message_data.join('\n');
+  fs.writeFile(fname, data_str, (err) => {
+    if (err) { throw err; }
+  });
+}
